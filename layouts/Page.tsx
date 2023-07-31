b592b67cd100dfbce1443db70a15b08a485c2c74
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-07-31 19:26:26 -0300

    [UPDATE] Add layout sizes to Page

--- layouts/Page.tsx
+++ layouts/Page.tsx
@@ -1,13 +1,19 @@
 interface Props {
 	children: React.ReactNode
 	className?: string
+	title?: string
+	description?: string
 }
 
 export default function Page({ children, className = '' }: Props) {
 	return (
-		<div>
+		<div className='bg-gray-100 p-8 h-screen max-md:p-0 '>
 			<header></header>
-			<main className={`h-screen p-6 ${className}`}>{children}</main>
+			<main
+				className={`h-full rounded-xl bg-white w-full max-w-md max-md:max-w-none max-md:rounded-none mx-auto p-6 ${className}`}
+			>
+				{children}
+			</main>
 		</div>
 	)
 }
