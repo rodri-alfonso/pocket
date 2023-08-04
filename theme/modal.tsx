0701db1910e3f2fe4c17106e434c8c6794672eca
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-08-03 23:30:34 -0300

    [FEATURE]: Create Modal component

---
+++ theme/modal.tsx
@@ -0,0 +1,18 @@
+interface Props {
+	isOpen: boolean
+	onClose: () => void
+	children: React.ReactNode
+	className?: string
+}
+
+export default function Modal({ children, isOpen, onClose, className = '' }: Props) {
+	return (
+		<dialog
+			open={isOpen}
+			onClose={onClose}
+			className='bg-black bg-opacity-80 absolute top-0 grid place-items-center w-full h-full transition-all'
+		>
+			<section className={`w-full max-w-sm rounded-xl h-1/2 bg-white p-6 ${className}`}>{children}</section>
+		</dialog>
+	)
+}
