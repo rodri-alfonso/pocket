00f99cb987d062963d02a302e99b188976e0b7b3
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-08-14 19:00:27 -0300

    [UPDATE] Improve and clear create page

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,61 +1,12 @@
 import Page from '@/layouts/Page'
-import Input from '@/theme/Input'
-import { useState } from 'react'
-import Router from 'next/router'
-import { db } from '@/firebase-config'
-import { collection, addDoc } from 'firebase/firestore'
-import { Planning } from '@/types/planning'
-import { useRegistration } from '@/context/planning'
-import ButtonDouble from '@/theme/button-double'
 import Content from '@/layouts/Content'
+import PlanningForm from '@/components/PlanningForm'
 
 export default function Create() {
-	const [planningName, setPlanningName] = useState('')
-	const { user, setRegistration } = useRegistration()
-
-	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault()
-		if (!planningName) return console.log('Please fill out all fields')
-
-		const payload: Planning = {
-			hostId: user.id,
-			participants: [{ name: user.name, vote: 0, avatar: 'F1', id: user.id }],
-			host: user.name,
-			average: 0,
-			name: planningName,
-			votingSystem: 'fibonacci',
-		}
-
-		addDoc(collection(db, 'plannings'), payload)
-			.then((response) => {
-				Router.push(`/planning/${response.id}`)
-			})
-			.catch((error) => {
-				console.log(error)
-			})
-	}
-
 	return (
 		<Page>
-			<Content>
-				<form onSubmit={handleSubmit} className='flex flex-col justify-between h-full'>
-					<div className='flex flex-col gap-8 items-center'>
-						<h1 className='font-medium text-gray-600 text-lg'>Let's create a planning</h1>
-						<Input
-							label='Planning Name'
-							placeholder='Enter a planning name'
-							value={planningName}
-							onChange={(e) => setPlanningName(e.target.value)}
-						/>
-					</div>
-					<ButtonDouble
-						labelPrimary='Create'
-						labelSecondary='Cancel'
-						disabled={!planningName}
-						onClickPrimary={() => {}}
-						onClickSecondary={() => Router.push('/')}
-					/>
-				</form>
+			<Content className='place-items-center grid'>
+				<PlanningForm />
 			</Content>
 		</Page>
 	)
--- theme/Input.tsx
+++ theme/Input.tsx
@@ -10,7 +10,12 @@ export default function Input({ label, ...props }: Props) {
 	return (
 		<div className='grid gap-2 w-full text-center'>
 			<label className='font-semibold text-gray-500'>{label}</label>
-			<input type='text' className='border-2 border-gray-400 rounded-lg w-full p-2 px-4' required {...props} />
+			<input
+				type='text'
+				className='border-2 border-gray-400 rounded-lg w-full transition-all p-2 px-4 focus-visible:outline-none focus-visible:ring-1 focus-visible:border-gray-900 focus-visible:ring-gray-800 '
+				required
+				{...props}
+			/>
 		</div>
 	)
 }
