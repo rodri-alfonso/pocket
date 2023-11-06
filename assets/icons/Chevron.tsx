aa80e3b30751df1e4d3b356b344c227fcf372e65
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-11-06 13:31:33 -0300

    [UPDATE]: Restyle PlanningCard and add icons

---
+++ assets/icons/Chevron.tsx
@@ -0,0 +1,14 @@
+interface Props {
+	className?: string
+}
+
+export default function ChevronIcon({ className }: Props) {
+	return (
+		<svg width='7' height='12' viewBox='0 0 7 12' fill='none' className={className}>
+			<path
+				d='M0.2925 0.293762C-0.0975 0.683762 -0.0975 1.31376 0.2925 1.70376L4.1725 5.58376L0.2925 9.46376C-0.0975 9.85376 -0.0975 10.4838 0.2925 10.8738C0.6825 11.2638 1.3125 11.2638 1.7025 10.8738L6.2925 6.28376C6.6825 5.89376 6.6825 5.26376 6.2925 4.87376L1.7025 0.283762C1.3225 -0.0962379 0.6825 -0.0962377 0.2925 0.293762Z'
+				fill='currentColor'
+			/>
+		</svg>
+	)
+}
--- components/PlanningCard.tsx
+++ components/PlanningCard.tsx
@@ -12,25 +12,27 @@ export default function PlanningCard({ id, name, participants, votingSystem }: P
 	return (
 		<Link
 			href={`/planning/${id}`}
-			className='flex bg-white justify-between h-full rounded-2xl text-gray-800 border-solid border border-gray-200 p-6 w-full active:scale-95 transition-all  hover:shadow-md'
+			className='flex bg-gray-800 justify-between h-full rounded-2xl text-white border-solid border border-gray-200 p-6 w-full active:scale-95 transition-all  hover:shadow-md'
 		>
 			<div className='grid items-center gap-6'>
-				<h2 className='font-semibold'>{planningName}</h2>
+				<h2 className='font-semibold '>{planningName}</h2>
 
 				<div className='flex items-center gap-2'>
 					<div className='flex items-center '>
 						{participants.map((participant, index) => (
-							<F1 key={`${index}_${participant}`} className='w-8 h-8  rounded-full -ml-1.5 bg-white' />
+							<F1 key={`${index}_${participant}`} className='w-8 h-8 text-black  rounded-full -ml-1.5 bg-gray-100' />
 						))}
 					</div>
 				</div>
 			</div>
 
 			<div className='flex flex-col items-end justify-between h-full'>
-				<button className='border border-solid hover:bg-gray-800 transition-all hover:text-white border-gray-300 rounded-full  w-8 h-8 grid place-items-center font-semibold text-xs '>
-					{'>'}
-				</button>
-				<span className='bg-gray-100 text-gray-500 font-medium text-sm px-2 py-0.5 rounded-full'>{votingSystem}</span>
+				<span className='border border-solid border-gray-300 text-gray-300 font-medium text-xs px-2.5 py-0.5 rounded-full'>
+					{votingSystem}
+				</span>
+				<span className='border border-solid hover:bg-gray-800 text-gray-800 bg-white transition-all hover:text-white border-gray-300 rounded-full py-0.5 px-3 grid place-items-center font-semibold text-sm'>
+					Join
+				</span>
 			</div>
 		</Link>
 	)
