18ff732c42fb4d6b4b30f3ea57b548b1bebf85d0
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2023-11-07 21:16:51 -0300

    [UPDATE]: Add testing pages v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { PlanningProvider } from '@/context/planning'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		document.documentElement.style.setProperty('--doc-height', `${window.innerHeight}px`)
+	}, [])
+
 	return (
 		<PlanningProvider>
 			<Component {...pageProps} />
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,6 @@
 export default function TestPage() {
 	return (
-		<div className='h-screen p-4'>
+		<div className='h-[var(--doc-height)] p-4'>
 			<div className='h-full bg-blue-200 rounded-lg'>
 				<h1>I'm the TestPage</h1>
 			</div>
